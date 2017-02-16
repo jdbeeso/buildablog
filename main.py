e6eb9c7768053164fcc7ab789ad6d7b777bed3f8
jdbeeso <jdbeeso@gmail.com>
2017-02-16 16:13:20 -0600

    added database elements

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Art(db.model):
+class Art(db.Model):
     title = db.StringProperty(required = True)
     art = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
 
 class MainHandler(Handler):
     def render_front(self, title='', art='', error=''):
-        self.render('front.html', title=title, art=art, error=error)
+        arts=db.GqlQuery('SELECT * FROM Art ORDER By created DESC')
+        self.render('front.html', title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render('front.html')
@@ -51,10 +52,12 @@ class MainHandler(Handler):
         art = self.request.get('art')
 
         if title and art:
-            self.write('thanks!')
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect('/')
         else:
             error = "we need both a title and some artwork!"
             self.render_front(title, art, error)
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
 ], debug=True)
